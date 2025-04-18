53ca69b938de050c79a089f4ca2cd3cf93906e4d
Leonna <koicaaaaaa@gmail.com>
2025-04-18 11:15:57 -0400

    Finished with the js for new RPS, will return to work on the CSS in the future

--- rps.js
+++ rps.js
@@ -1,6 +1,27 @@
 let humanscore = 0;
 let computerscore = 0;
-let rounds = 5;
+
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const display = document.querySelector(".display");
+const score = document.querySelector(".score-board");
+const MAX_SCORE = 5;
+
+display.textContent = `Welcome to RPS`;
+
+rock.addEventListener("click", () => {
+    playGame("rock");
+})
+
+paper.addEventListener("click", () => {
+    playGame("paper");
+})
+
+scissors.addEventListener("click", () => {
+    playGame("scissors");
+})
+
 
 function getComputerChoice() {
     a = Math.random();
@@ -15,60 +36,43 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let b = prompt("What will you choose? ");
-    b = b.toLowerCase();
-
-    if (b == 'rock') {
-        return "rock";
-    }
-    else if (b == 'paper') {
-        return "paper";
-    }
-    else if (b == 'scissors') {
-        return "scissors";
-    }
-    else {
-        return 0;
-    }
+function updateScore() {
+    score.textContent = `Human: ${humanscore} Computer: ${computerscore}`;
 }
+
 
 function playRound(computer, human) {
     if ((computer == "rock" && human == "scissors") ||
     (computer == "scissors" && human == "paper") ||
     (computer == "paper" && human == "rock")) {
-        console.log("Computer wins! " + computer + " beats " + human);
         computerscore++;
+        return (display.textContent = `Computer wins! ${computer} beats ${human}.`)
     }
     else if (computer == human) {
-        console.log("It's a tie.");
+        return (display.textContent = "It's a tie.");
     }
     else {
-        console.log("You win! " + human + " beats " + computer);
         humanscore++;
-    }
-    
-    return
-}
-
-function myGame() {
-
-    for (let i = 0; i < 5; i++) {
-        playRound(getComputerChoice(), getHumanChoice());
-        console.log("Your score is: ", humanscore);
-        console.log("The computer score is: ", computerscore);
-    }
-
-    if (humanscore > computerscore) {
-        console.log("You win!");
-    }
-    else if (computerscore > humanscore) {
-        console.log("Computer wins!");
-    }
-    else {
-        console.log("Tie!");
+        return (display.textContent = `You win! ${human} beats ${computer}.`)
     }
 }
 
-myGame()
-console.log(Hello)
+function playGame(human) {
+    computer = getComputerChoice();
+    playRound(computer, human);
+    updateScore();
+
+    if (humanscore == 5) {
+        display.textContent = `You win!`;
+        resetGame();
+    }
+    else if (computerscore == 5) {
+        display.textContent = `You lose!`;
+        resetGame();
+    }
+}
+
+function resetGame() {
+    humanscore = 0;
+    computerscore = 0;
+}
